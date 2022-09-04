7129a29d8104997868bac15b75357cd34472d6a0
Evan Zufah <cezufah@gmail.com>
2022-09-04 15:48:47 -0700

    assignment 2 complete!

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,18 @@
 import java.util.Scanner;
+/**Humans think in degrees, mathematicians think in radians - mathematicians not human confirmed
+ *
+ */
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
-
+        System.out.println("Please type the number of sides.");
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+        double b = 360.0/n;
+        double a = b/2;
+        double s = 2*Math.sin(Math.toRadians(a));
+        double p = n*s;
+        double pi = p/2;
+        System.out.printf("Our pi estimate is: %f", pi);
     }
 }
